bddea92df44330b8c11c64e83187e8552893aaf4
lans1q <skweezyyy@mail.ru>
2022-05-30 17:04:50 +0300

    feat(project): a bug fixed

--- src/components/FavoriteItem/style.ts
+++ src/components/FavoriteItem/style.ts
@@ -12,9 +12,9 @@ export const StyledLink = styled(Link)`
 `;
 
 export const StyledImageBlock = styled.div`
-  -webkit-box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2) inset;
-  -moz-box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2) inset;
-  box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2) inset;
+-webkit-box-shadow: 20px 15px 32px 0px rgba(9, 20, 28, 0.47);
+-moz-box-shadow: 20px 15px 32px 0px rgba(9, 20, 28, 0.47);
+box-shadow: 20px 15px 32px 0px rgba(9, 20, 28, 0.47);
 `;
 
 export const StyledImage = styled.img`
